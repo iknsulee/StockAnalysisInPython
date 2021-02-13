eaf03ac5781b2f769d817295135c082a33ea887f
KeunSuLee <i.knsulee@gmail.com>
2021-02-13 21:57:23 +0900

    fix to send header

--- 04_Web_Scraping/ch04_03_Celltrion_CandleChart_NewSchool.py
+++ 04_Web_Scraping/ch04_03_Celltrion_CandleChart_NewSchool.py
@@ -2,19 +2,27 @@ import pandas as pd
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import mplfinance as mpf
+import requests
 
 url = 'https://finance.naver.com/item/sise_day.nhn?code=068270&page=1'
-with urlopen(url) as doc:
-    html = BeautifulSoup(doc, 'lxml') 
-    pgrr = html.find('td', class_='pgRR')
-    s = str(pgrr.a['href']).split('=')
-    last_page = s[-1]  
+html = BeautifulSoup(requests.get(url, headers={'User-agent': 'Mozilla/5.0'}).text, "lxml")
+pgrr = html.find('td', class_='pgRR')
+s = str(pgrr.a['href']).split('=')
+last_page = s[-1]  
+
+# with urlopen(url) as doc:
+#     html = BeautifulSoup(doc, 'lxml') 
+#     pgrr = html.find('td', class_='pgRR')
+#     s = str(pgrr.a['href']).split('=')
+#     last_page = s[-1]  
 
 df = pd.DataFrame()
-sise_url = 'https://finance.naver.com/item/sise_day.nhn?code=068270'  
-for page in range(1, int(last_page)+1): 
+sise_url = 'https://finance.naver.com/item/sise_day.nhn?code=068270'
+# for page in range(1, int(last_page)+1): 
+for page in range(1, 31): # 다 가지고 오면 시간이 너무 오래 걸리니까 최근 30개만 가지고 온다.
     page_url = '{}&page={}'.format(sise_url, page)  
-    df = df.append(pd.read_html(page_url, header=0)[0])
+    # df = df.append(pd.read_html(page_url, header=0)[0])
+    df = df.append(pd.read_html(requests.get(page_url, headers={'User-agent': 'Mozilla/5.0'}).text)[0])
 
 df = df.dropna()
 df = df.iloc[0:30]
